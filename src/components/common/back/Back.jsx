aed1fa9ebe90542a6bb150686c77e3abb75fd838
zafer988 <zafer.celik06@hotmail.com>
2024-01-09 19:31:17 +0300

    first commit

--- src/components/common/back/Back.jsx
+++ src/components/common/back/Back.jsx
@@ -6,8 +6,6 @@ const Back = ({ title }) => {
         <>
             <section className='back'>
                 <h2>Home /{location.pathname.split('/')[1]}</h2>
-                {/* Bu, bir HTML başlık <h2> elementidir. İçeriği dinamik olarak oluşturulmuştur.
-                Home /{location.pathname.split('/')[1]} ifadesi, sayfanın URL yolu (path) içinden ikinci bölümü alır ve "Home /" metni ile birleştirir. Bu, bir başlık oluşturur */}
                 <h1>{title}</h1>
             </section>
             <div className='margin'></div>
